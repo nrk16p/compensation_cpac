f69ed2b70a24ec2d3cbdbb6687223ccaef558c3d
Menatransport_02 <menatransport_02@Menatransport-02s-MacBook-Air.local>
2026-04-07 17:02:48 +0700

    data

--- rmc_compensation.py
+++ rmc_compensation.py
@@ -173,12 +173,15 @@ def transform_data(df):
     vehicle_df = pd.json_normalize(vehicle["data"])
 
     # ฝั่ง df
-    df["รหัสรถ"] = df["รหัสรถ"].astype(int).astype(str)
+    df["รหัสรถ"] = (
+        pd.to_numeric(df["รหัสรถ"], errors="coerce")  # แปลงให้เป็นเลขก่อน
+        .astype(int)
+        .astype(str)
+    )
 
     # ฝั่ง vehicle
     vehicle_df["code"] = (
         pd.to_numeric(vehicle_df["code"], errors="coerce")
-        .fillna(0)
         .astype(int)
         .astype(str)
     )
